abac7db0eb709387c7331edd573b0c6c72f5b33f
Fredy Velásquez <59968764+fredyvelasquezgt@users.noreply.github.com>
2022-04-01 11:23:09 -0600

    Add files via upload

---
+++ src/pages/auth/promotionScreen.js
@@ -0,0 +1,10 @@
+import React from "react";
+//Pagina para promocionar los planes que vamos a tener
+
+export const promotionScreen = () => {
+  return (
+    <>
+      <h1>Promotion Page</h1>
+    </>
+  );
+};
